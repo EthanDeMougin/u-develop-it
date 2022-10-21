2b4c474aca87d00062c17b3aa74d7a31597bd13c
Ethan DeMougin <ethandemougin@gmail.com>
2022-10-21 15:47:54 -0400

    added voters table with correct voter data

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,16 @@
+/* Deletes table if an extra table of the respective id is created again, to avoid duplicate data and tables */
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
+DROP TABLE IF EXISTS voters;
 
+/* Creates table for parties */
 CREATE TABLE parties (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
   description TEXT
 );
 
+/* Creates table for candidates */
 CREATE TABLE candidates (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
@@ -14,4 +18,13 @@ CREATE TABLE candidates (
   party_id INTEGER,
   industry_connected BOOLEAN NOT NULL,
   CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
+);
+
+/* Creates table for voters */
+CREATE TABLE voters (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
